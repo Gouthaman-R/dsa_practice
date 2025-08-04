045614ef6baddcfb6113c5749bbec1444c478f2b
Gouthaman R <gouthamanravi2004@gmail.com>
2025-08-04 13:09:40 +0530

    practice question added-2

--- problems.py
+++ problems.py
@@ -48,7 +48,7 @@
 #                 print(i,end=" ")
 # prime(10,30)
 
-
+# =======================================================================================================
 
 # write a program which takes two values x and y. print x for y number of times
 
@@ -69,6 +69,8 @@
 # for i in range(b):
 #     print(a)
 
+
+# =======================================================================================================
 
 """Write a program to get firstName and lastName and n as input and print fullName that is firstName+lastName for n times.
 
@@ -102,6 +104,8 @@ NandyRaja"""
 #     print(d)
 
 
+# =======================================================================================================
+
 
 """Write a program to take x and print multiples of x till 1000.
 
@@ -133,9 +137,103 @@ Expected Output:
 
 # solution
 
-x = int(input("Enter value of x: "))
-n = 1
+# x = int(input("Enter value of x: "))
+# n = 1
 
-while x * n <= 1000:
-    print(x * n)
-    n += 1
+# while x * n <= 1000:
+#     print(x * n)
+#     n += 1
+
+
+# =======================================================================================================
+"""
+Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
+
+
+Input:  2 Name y
+
+Expected Output:
+
+2
+
+Name
+
+y
+
+"""
+#  solution 
+# a= (input("enter the number"))
+# b=(input("enter the name:"))
+# c=(input("enter the letter:"))
+# print(a,'\n'+b,'\n'+c)
+
+
+# =======================================================================================================
+
+"""
+Prob 2: Write a program to check whether a triangle can be formed with the given values for the angles.
+
+If sum of angles is equal to 180, then triangle can be formed, else it can't be formed.
+
+Input: 45 45 45
+
+Expected Output: 
+
+Triangle cannot be formed
+
+Explanation -> We are getting 3 inputs, that is three angles of triangle, but here the sum of three angles that is 45+45+45 is not equal to 180 so Triangle cannot be formed is the output.
+
+"""
+# solution
+
+# def triangle(a,b,c):
+#     sum=a+b+c
+#     if sum==180:
+#         print("can form triangle")
+#     else:
+#         print("cannot form triangle")
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# c=int(input("enter the number:"))
+# triangle(a,b,c)
+
+
+# =======================================================================================================
+
+"""Given mark of student, Print the Grade
+
+Grade A if mark is greater than or equal to 90
+
+Grade B if mark is greater than or equal to 80
+
+Grade C if mark if greater than or equal to 60
+
+Grade D if mark if greaer than or equal to 35
+
+Fail if mark is lesser than 35
+
+
+Input: 95
+
+Expected Output:
+
+Grade A
+"""
+
+# solution
+
+# mark=int(input("enter the mark:"))
+# if mark>=90 and mark<=100:
+#     print("grade A")
+# elif mark>=80 and mark<90:
+#     print("grade B")
+# elif mark>=60 and mark<80:
+#     print("grade C")
+# elif mark>=35 and mark<60:
+#     print("grade D")
+# elif mark<35 and mark>=0:
+#     print("fail")
+# else:
+#     print("invalid mark")
+
+# =======================================================================================================
