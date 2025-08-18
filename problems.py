2cfc99851ee976f4db5a780dbdc45ebc16197479
Gouthaman R <gouthamanravi2004@gmail.com>
2025-08-18 14:55:43 +0530

    new problems added

--- problems.py
+++ problems.py
@@ -661,3 +661,370 @@ Grade A
     
 
 # =============================================================================================================
+
+"palindrome of the number"
+# example 1
+# n=11
+# b=n
+# sum=0
+# while(n>0):
+#     ld=n%10
+#     sum=sum*10+ld
+#     n//=10
+# if sum==b:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+
+# example 2
+
+# n=int(input("enter the number:"))
+# d=str(n)
+# pal=d[::-1]
+# doc=int(pal)
+# if n==doc:
+    
+#   print("it is a palindrome")
+# else:
+#   print("it is not a palindrome")
+
+# output:
+
+"""enter the number:123321
+it is a palindrome """
+
+# =============================================================================================================
+
+"to print reverse of the number"
+# example 1
+
+# n=12345
+# while(n>0):
+#     ld=n%10
+#     print(ld,end="")
+#     n//=10
+
+
+# example 2
+
+# n=12345
+# sum=0
+# while(n>0):
+#     ld=n%10
+#     sum=sum*10+ld
+#     n//=10
+# print(sum)
+
+# output
+
+" 54321 "
+
+# =============================================================================================================
+
+" to find the factorial of the number"
+
+# n=int(input("enter the number:"))
+# fact=1
+# for i in range(1,n+1):
+#   fact=fact*i
+# print(f"The factorial of {n} is :{fact}")
+
+# output
+
+""" 
+enter the number:5
+The factorial of5 is :120
+
+"""
+
+# =============================================================================================================
+"to check the number is prime or not "
+
+
+# def prime(n):
+    
+#   if n<=1:
+#     return False
+#   for i in range(2,int(n**0.5)+1):
+#     if n%i==0:
+#       return False
+  
+#   return True
+  
+# print(prime(3))
+
+# outout:
+"True "
+
+
+# def prime(num1,num2):
+#     for num in range(num1,num2+1):
+#         if num>1:
+#             for i in range(2,int(num**0.5)+1):
+#                 if num%i==0:
+#                     break
+#             else:
+#                 print(num,end=" ")
+# prime(1,10)
+
+# outout:
+"2 3 5 7 "
+
+
+# def prime(n):
+#     for num in range(2,n+1):
+#         check=True
+#         for i in range(2,int(num**0.5)+1):
+#             if num%i==0:
+#                 check = False
+#                 break
+#         else:
+#             print(num,end=" ")
+# prime(20)
+            
+# outout:
+" 2 3 5 7 11 13 17 19 "
+
+# =============================================================================================================
+
+"""
+Given an integer n, find the number of divisors of n that are divisible by 3.
+
+Examples:
+
+Input: n = 6
+Output: 2
+Explanation: 1, 2, 3, 6 are divisors of 6 out of which 3 and 6 are divisible by 3.
+
+"""
+# n=15
+# count=0
+# for i in range(1,n+1):
+#     if n%i==0:
+        
+#         if i%3==0:
+            
+#           count=count+1
+# print(count)
+
+# output:
+" 2 "
+
+# =============================================================================================================
+"to find the trailing zeros of the factorial"
+"explanation: 5!=120. the number of zeros in the end of the factorial is 1."
+
+# brute force program
+
+# n=12
+# fact=1
+# count=0
+# for i in range(1,n+1):
+#     fact=fact*i
+# while(fact>0):
+#     ld=fact%10
+#     if ld==0:
+#         count=count+1
+#     else:
+#         break
+#     fact//=10
+# print(count)
+
+
+# optimized program
+
+# n = 12
+# count = 0
+# i = 5
+# while n // i > 0:
+#     count += n // i
+#     i *= 5
+# print(count)
+
+# output;
+"2"
+
+# =============================================================================================================
+"To find the GCD of the two numbers"
+
+
+# def gcd(a,b):
+#     if a>=b:
+#       gcd_value=1
+#       for i in range(2,a+1):
+#           if a%i==0 and b%i==0:
+#               gcd_value=i
+#       return gcd_value
+#     elif b>a:
+#       gcd_value=1
+#       for i in range(2,b+1):
+#           if a%i==0 and b%i==0:
+#               gcd_value=i
+#       return gcd_value
+# a=int(input("Enter the number:"))
+# b=int(input("Enter the number:"))
+# print("the gcd of these number is ",gcd(a,b))
+
+# output
+"""
+Enter the number:15
+Enter the number:25
+the gcd of these number is  5
+"""
+
+# =============================================================================================================
+"to find the middle value of the given numbers"
+
+# example 1
+
+# def middle(a,b,c):
+#     nums=[a,b,c]
+#     sorted_num=sorted(nums)
+#     mid_value=sorted_num[1]
+#     return mid_value
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=int(input("Enter a number:"))
+# print(f"the middle value is:{middle(a,b,c)}")
+
+# output
+
+"""
+Enter a number:500
+Enter a number:600
+Enter a number:100
+the middle value is:500
+
+"""
+# example 2
+
+# def mid(a,b,c):
+#     if a>b and a>c:
+#         return b if b>c else c
+#     elif b>a and b>c:
+#         return a if a>c else c
+#     elif c>a and c>b:
+#         return b if b>a else a
+# print(mid(100,299,500))
+
+
+# =============================================================================================================
+
+"""Given the value of n, we need to find the sum of the series where i-th term is sum of first i natural numbers.
+
+NOTE: Sum of the series 1 + (1+2) + (1+2+3) + (1+2+3+4) + …… + (1+2+3+4+…+n)
+
+Example 1:
+
+Input: n = 5
+Output: 35 
+Explanation: 1 + (1+2) + (1+2+3).. = 35
+Hence sum of the series is 35."""
+
+# n=7
+# count=0
+# sum=0
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         count=j+count
+#     sum=count+sum
+#     count=0
+# print(sum)
+
+# output
+
+" 84 "
+
+# optimized program
+
+# def sumOfTheSeries (n):
+     
+#       return (n * (n + 1) * (n + 2)) // 6 
+# print(sumOfTheSeries(7))
+
+# =============================================================================================================
+
+"""Given an integer n, your task is to compute the sum of all natural numbers from 1 to n (inclusive). If n is 0, the sum should be 0.
+
+Examples:
+
+Input: n = 3
+Output: 6
+Explanation: For n = 3, the sum will be 6. 1 + 2 + 3 = 6."""
+
+# n=3
+# sum=0
+# for i in range(1,n+1):
+#     sum=sum+i
+# print(sum)
+
+# optimized program
+
+# def findSum( n):
+#   return n*(n+1)//2
+
+# output
+"6"
+
+# =============================================================================================================
+"""Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+
+Examples:
+
+Input: n = 5
+Output: 225
+Explanation: 13 + 23 + 33 + 43 + 53 = 225 --> 1 pow 3,2 pow 3, 3 pow 3 ...""" 
+
+# n=7
+# sum=0
+# for i in range(1,n+1):
+#     power=pow(i,3)
+#     sum=sum+power
+# print(sum)
+
+# output
+"784"
+
+# optimized program
+
+# def sumOfSeries( n):
+#   return ((n * (n + 1)) // 2) ** 2
+
+# =============================================================================================================
+
+"""Given an integer N, find the absolute difference between sum of the squares of first N natural numbers and square of sum of first N natural numbers.
+
+Example 1:
+
+Input: N = 2
+Output: 4 
+Explanation: abs|(1**2 + 2**2) - (1 + 2)**2| = 4."""
+
+# n=3
+# count=0
+# sum=0
+
+# for i in range(1,n+1):
+#     power=pow(i,2)
+#     sum=sum+power
+
+# for j in range(1,n+1):
+#     count=count+j
+# count_power=pow(count,2)
+
+# print(abs(sum-count_power))
+
+# output
+" 22 "
+
+# optimized program
+
+# def squaresDiff (N):
+#         sum_of_squares = (N * (N + 1) * (2 * N + 1)) // 6
+#         square_of_sum = ((N * (N + 1)) // 2) ** 2
+#         return abs(sum_of_squares - square_of_sum) 
+
+# =============================================================================================================
+
